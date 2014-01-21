4972a3e8a8ebd4953d1b0acf8b76684801b9c9f0
Sean Maraj <seanmaraj@live.com>
2014-01-20 19:47:47 -0500

    Changes before upgrade

---
+++ SimpleShedule/src/ScheduleEntryField.java
@@ -0,0 +1,17 @@
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+
+
+public class ScheduleEntryField extends View{
+
+	public ScheduleEntryField(Context context) {
+		super(context);
+		
+		
+		//Button button = new Button(this); 
+	}
+
+	
+	
+}
--- SimpleShedule/src/com/stmaraj/simpleshedule/Schedules.java
+++ SimpleShedule/src/com/stmaraj/simpleshedule/Schedules.java
@@ -1,14 +1,26 @@
 package com.stmaraj.simpleshedule;
 
 import android.os.Bundle;
+import android.R.integer;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.Toast;
 
 public class Schedules extends Activity {
+	
+	
+	TableLayout tableLayout1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+	    tableLayout1 = (TableLayout) findViewById(R.id.TableLayout1);
+
+		
+		
 		setContentView(R.layout.activity_schedules);
 	}
 
@@ -17,6 +29,21 @@ public class Schedules extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.schedules, menu);
 		return true;
+	}
+	
+	public void btnAddClick(View v)
+	{
+		int i = v.getId();
+		int kj = R.id.btnAdd;
+		
+		if (i == kj)
+		{
+		Toast toast = Toast.makeText(this, i, Toast.LENGTH_LONG);
+		toast.show();
+		}
+		
+		//tableLayout1.addv
+		
 	}
 
 }
