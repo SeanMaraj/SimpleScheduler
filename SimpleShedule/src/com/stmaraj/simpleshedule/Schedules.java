a9081f7eb75149d8d166778d7f2c311f6e42ca55
Sean Maraj <seanmaraj@live.com>
2014-02-06 19:35:45 -0500

    Added Date Picker Dialog

--- SimpleShedule/src/com/stmaraj/simpleshedule/Schedules.java
+++ SimpleShedule/src/com/stmaraj/simpleshedule/Schedules.java
@@ -1,27 +1,33 @@
 package com.stmaraj.simpleshedule;
 
+
+import java.util.GregorianCalendar;
 import android.os.Bundle;
-import android.R.integer;
 import android.app.Activity;
-import android.util.AttributeSet;
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.text.format.DateFormat;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
+import android.widget.DatePicker;
 import android.widget.RelativeLayout;
-import android.widget.TableLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Schedules extends Activity implements OnClickListener {
 	
+	RelativeLayout relativeLayout;
 	ScheduleEntryField firstSchedule;
 	Button btnAdd;
-	RelativeLayout relativeLayout;
+	TextView txtDate;
 	int id = 1001;
+	int year = 0;
+	int monthOfYear = 0;
+	int dayOfMonth = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +35,15 @@ public class Schedules extends Activity implements OnClickListener {
 		setContentView(R.layout.activity_schedules);
 		
 		relativeLayout = (RelativeLayout)findViewById(R.id.RelativeLayoutSchedules2);
+		txtDate = (TextView)findViewById(R.id.txtDate);		
 		
 		// set id so future schedules can stack
 		firstSchedule = (ScheduleEntryField)findViewById(R.id.firstSchedule);
 		firstSchedule.setId(1000);
-
+		
 		// create add button and place below first schedule
 		btnAdd = new Button(this);
+		btnAdd.setId(100);
 		btnAdd.setOnClickListener(this);
 		btnAdd.setGravity(Gravity.CENTER);
 		RelativeLayout.LayoutParams btnAddParams = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
@@ -52,7 +60,21 @@ public class Schedules extends Activity implements OnClickListener {
 
 	@Override
 	public void onClick(View v) {
-		ScheduleEntryField schedule = new ScheduleEntryField(this);
+		
+		switch( v.getId() )
+		{
+			case 100:
+				addScheduleField();
+				break;
+			case R.id.txtDate:
+				setDate();
+				break;
+		}
+	}
+	
+    public void addScheduleField()
+    {
+    	ScheduleEntryField schedule = new ScheduleEntryField(this);
 		schedule.setId(id);
 		
 		//set length and width of schedule 
@@ -66,6 +88,27 @@ public class Schedules extends Activity implements OnClickListener {
 		btnAdd.setLayoutParams(layoutParams);
 		
 		id++;
-	}
+    }
+    
+    public void setDate() 
+    {
+		DatePickerDialog datePickerDialog = new DatePickerDialog(this, dateSetListener, year, monthOfYear, dayOfMonth);
+		datePickerDialog.show();
+    }
+    
+    private OnDateSetListener dateSetListener =  new OnDateSetListener() {
+		
+		@Override
+		public void onDateSet(DatePicker view, int year, int monthOfYear,
+				int dayOfMonth) {
+			
+			Schedules.this.year = year;
+			Schedules.this.monthOfYear = monthOfYear;
+			Schedules.this.dayOfMonth = dayOfMonth;
+			
+			GregorianCalendar date = new GregorianCalendar(year, monthOfYear, dayOfMonth);
+			txtDate.setText(DateFormat.format("MMMM dd,  yyyy", date));
+		}
+    };
 
 }
